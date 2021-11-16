6922a585cae9ef0cc23e2c05e80e87ee5d1d7177
Clayton Gorham <claytongorham@gmail.com>
2021-11-15 19:27:59 -0800

    updates

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+
+let answer = window.prompt(weeklyExpenseQuestions[0]);
+console.log(typeof answer)
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++) {
+    let answer = window.prompt(weeklyExpenseQuestions[i]);
+    weeklyExpenses = weeklyExpenses + parseInt(answer);
+}
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++) {
+    let answer = window.prompt(monthlyExpenseQuestions[i]);
+    monthlyExpenses = monthlyExpenses + parseInt(answer);
+}
+
+for(let i = 0; i < annualExpenseQuestions.length; i++) {
+    let answer = window.prompt(annualExpenseQuestions[i]);
+    annualExpenses = annualExpenses + parseInt(answer);
+}
